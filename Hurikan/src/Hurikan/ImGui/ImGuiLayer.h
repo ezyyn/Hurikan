9595e53516a52157195eced369161cc862938d84
RikalSiHurikan <ezyn69@gmail.com>
2021-09-13 23:29:20 +0200

    Update ImGuiLayer

--- Hurikan/src/Hurikan/ImGui/ImGuiLayer.h
+++ Hurikan/src/Hurikan/ImGui/ImGuiLayer.h
@@ -11,6 +11,9 @@ namespace Hurikan {
 
 		virtual void OnAttach() override;
 		virtual void OnDetach() override;
+
+		virtual void OnEvent(Event& e) override;
+
 		void Begin();
 		void End();
 	private:
