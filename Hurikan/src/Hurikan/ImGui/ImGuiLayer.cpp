730929bd8a9439e00753ca764cf2f597786aadfe
RikalSiHurikan <ezyn69@gmail.com>
2021-09-26 23:53:31 +0200

    Update ImGuiLayer.cpp

--- Hurikan/src/Hurikan/ImGui/ImGuiLayer.cpp
+++ Hurikan/src/Hurikan/ImGui/ImGuiLayer.cpp
@@ -7,42 +7,46 @@
 
 #include <GLFW/glfw3.h>
 
+#pragma warning(push, 0)
 #include "examples/imgui_impl_opengl3.h"
 #include "examples/imgui_impl_glfw.h"
+#pragma warning(pop)
 
 #include "Hurikan/Core/Application.h"
 
 
 namespace Hurikan {
 
-	ImGuiLayer::ImGuiLayer() : Layer("ImGuiLayer")
-	{	
-	}
-
-	ImGuiLayer::~ImGuiLayer()
+	ImGuiLayer::ImGuiLayer()
+		: Layer("ImGuiLayer")
 	{
 	}
 
-	void ImGuiLayer::OnAttach() 
+	void ImGuiLayer::OnAttach()
 	{
 		HU_PROFILE_FUNCTION();
 
-		//Setup Dear ImGui context
+		// Setup Dear ImGui context
 		IMGUI_CHECKVERSION();
 		ImGui::CreateContext();
-	
-		ImGuiIO& io = ImGui::GetIO();
-		io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
+		ImGuiIO& io = ImGui::GetIO(); (void)io;
+		io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;       // Enable Keyboard Controls
+		//io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+		io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;           // Enable Docking
+		io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;         // Enable Multi-Viewport / Platform Windows
+		//io.ConfigFlags |= ImGuiConfigFlags_ViewportsNoTaskBarIcons;
+		//io.ConfigFlags |= ImGuiConfigFlags_ViewportsNoMerge;
 
-		io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;
-		io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;
+		io.Fonts->AddFontFromFileTTF("assets/fonts/Open_Sans/static/OpenSans/OpenSans-Bold.ttf", 18.0f);
+		io.FontDefault = io.Fonts->AddFontFromFileTTF("assets/fonts/Open_Sans/static/OpenSans/OpenSans-Regular.ttf", 18.0f);
 
+		// Setup Dear ImGui style
+		ImGui::StyleColorsDark();
+		//ImGui::StyleColorsClassic();
 
-		io.Fonts->AddFontFromFileTTF("assets/fonts/Open_Sans/static/OpenSans/OpenSans-Bold.ttf",18.0f);
-		io.FontDefault = io.Fonts->AddFontFromFileTTF("assets/fonts/Open_Sans/static/OpenSans/OpenSans-Regular.ttf",18.0f);
-
+		// When viewports are enabled we tweak WindowRounding/WindowBg so platform windows can look identical to regular ones.
 		ImGuiStyle& style = ImGui::GetStyle();
-		if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable) 
+		if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
 		{
 			style.WindowRounding = 0.0f;
 			style.Colors[ImGuiCol_WindowBg].w = 1.0f;
@@ -52,14 +56,16 @@ namespace Hurikan {
 
 		Application& app = Application::Get();
 		GLFWwindow* window = static_cast<GLFWwindow*>(app.GetWindow().GetNativeWindow());
-		 
-		//Setup Platform/Renderer bindings
+
+		// Setup Platform/Renderer bindings
 		ImGui_ImplGlfw_InitForOpenGL(window, true);
 		ImGui_ImplOpenGL3_Init("#version 410");
 	}
-	void ImGuiLayer::OnDetach() {
+
+	void ImGuiLayer::OnDetach()
+	{
 		HU_PROFILE_FUNCTION();
-		
+
 		ImGui_ImplOpenGL3_Shutdown();
 		ImGui_ImplGlfw_Shutdown();
 		ImGui::DestroyContext();
@@ -92,11 +98,11 @@ namespace Hurikan {
 		Application& app = Application::Get();
 		io.DisplaySize = ImVec2((float)app.GetWindow().GetWidth(), (float)app.GetWindow().GetHeight());
 
-		//Rendering
+		// Rendering
 		ImGui::Render();
 		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 
-		if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable) 
+		if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
 		{
 			GLFWwindow* backup_current_context = glfwGetCurrentContext();
 			ImGui::UpdatePlatformWindows();
